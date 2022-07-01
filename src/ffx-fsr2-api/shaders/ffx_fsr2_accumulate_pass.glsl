f3b205ff8930ca9a117dd232af402096deb718ac
Hans-Kristian Arntzen <post@themaister.net>
2022-07-01 23:53:50 +0200

    glsl: Use correct format qualifiers and use EXT when appropriate.
    For user buffers, load-store without format is required.
    The other internal buffers just use something random, which is not correct.

--- src/ffx-fsr2-api/shaders/ffx_fsr2_accumulate_pass.glsl
+++ src/ffx-fsr2-api/shaders/ffx_fsr2_accumulate_pass.glsl
@@ -42,6 +42,7 @@
 
 #extension GL_GOOGLE_include_directive : require
 #extension GL_EXT_samplerless_texture_functions : require
+#extension GL_EXT_shader_image_load_formatted : require
 
 #define FSR2_BIND_SRV_EXPOSURE                               0
 #define FSR2_BIND_SRV_TRANSPARENCY_AND_COMPOSITION_MASK      1
