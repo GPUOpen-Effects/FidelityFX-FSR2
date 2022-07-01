584457150e86d2183f74702c72a51a76abb6ff99
Hans-Kristian Arntzen <post@themaister.net>
2022-07-02 00:06:51 +0200

    Handle dilated depth more gracefully for non-inverted depth.
    Encode inverted range in FP16.

--- src/ffx-fsr2-api/shaders/ffx_fsr2_callbacks_glsl.h
+++ src/ffx-fsr2-api/shaders/ffx_fsr2_callbacks_glsl.h
@@ -624,6 +624,9 @@ void SetReconstructedDepth(FFX_MIN16_I2 iPxSample, FfxUInt32 uValue)
 void StoreDilatedDepth(FFX_PARAMETER_IN FFX_MIN16_I2 iPxPos, FFX_PARAMETER_IN FfxFloat32 fDepth)
 {
 #if defined(FSR2_BIND_UAV_DILATED_DEPTH)
+#if !FFX_FSR2_OPTION_INVERTED_DEPTH
+	fDepth = 1.0 - fDepth; // Preserve precision as well as we can in FP16.
+#endif
 	//FfxUInt32 uDepth = f32tof16(fDepth);
 	imageStore(rw_dilatedDepth, iPxPos, vec4(fDepth, 0.0f, 0.0f, 0.0f));
 #endif
@@ -658,7 +661,11 @@ FfxFloat32x2 SampleDilatedMotionVector(FfxFloat32x2 fUV)
 FfxFloat32 LoadDilatedDepth(FFX_MIN16_I2 iPxInput)
 {
 #if defined(FSR2_BIND_SRV_DILATED_DEPTH)
-	return texelFetch(r_dilatedDepth, iPxInput, 0).r;
+	FfxFloat32 fDepth = texelFetch(r_dilatedDepth, iPxInput, 0).r;
+#if !FFX_FSR2_OPTION_INVERTED_DEPTH
+	fDepth = 1.0 - fDepth; // Reconstruct from FP16.
+#endif
+	return fDepth;
 #else
     return 0.f;
 #endif
